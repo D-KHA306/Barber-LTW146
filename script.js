774606186c0dfd23d31246bdc317dd1dc7165fa4
D-KHA306 <96805223+D-KHA306@users.noreply.github.com>
2021-12-29 19:23:08 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const menubarBtn = document.querySelector(".header-top img")
+menubarBtn.addEventListener("click", function(){
+    document.querySelector(".header-top ul").classList.toggle("active")
+})   
+                        
